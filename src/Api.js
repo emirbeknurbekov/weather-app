f986f913af9f570d591d17693b9b7e93f6cd78fd
emirbeknurbekov <emirbek.nurbekov@alatoo.edu.kg>
2023-12-13 16:06:41 +0600

    ready

---
+++ src/Api.js
@@ -0,0 +1,12 @@
+export const geoApiOptions = {
+  method: "GET",
+  headers: {
+    "X-RapidAPI-Key": "7a520b669emsh3d1f3a4adfb920dp15233djsn95585d200b66",
+    "X-RapidAPI-Host": "wft-geo-db.p.rapidapi.com",
+  },
+};
+
+export const GEO_API_URL = "https://wft-geo-db.p.rapidapi.com/v1/geo";
+
+export const WEATHER_API_URL = "https://api.openweathermap.org/data/2.5";
+export const WEATHER_API_KEY = "d14b1921014e713e8f2de51951520ff6";
